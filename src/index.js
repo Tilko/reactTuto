b150dba1ade434541e333c45defe179c1b2b0234
gmart <martinetti.gregoire@gmail.com>
2020-07-12 18:48:05 +0200

    add history buttons

--- src/index.js
+++ src/index.js
@@ -77,6 +77,21 @@ class Game extends React.Component {
             status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
         }
 
+
+        const moves = history.map((step, move) => {
+            const desc = move
+                ? 'Revenir au tour n°' + move
+                : 'Revenir au début de la partie';
+            return (
+                <li >
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
+
+
         return (
             <div className="game">
                 <div className="game-board">
@@ -87,7 +102,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
